cf2ab5d13c3105ad60f0cb363f918538c084601c
Beelizath <64615717+lucasffari@users.noreply.github.com>
2026-09-23 16:43:36 -0300

    Add files via upload

---
+++ questoes/a4t2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+  float preco, resultado;
+  char condicao;
+
+  printf("Digite o preço do produto: ");
+  scanf("%f", &preco);
+  printf("Digite a condição de pagamento: "); //problema aqui
+  scanf("%c", &condicao);
+  switch(condicao){
+    case 1: resultado = preco * 0.9; break;
+    case 2: resultado = preco * 0.95; break;
+    case 3: resultado = preco; break;
+    case 4: resultado = preco * 1.1; break;} 
+  printf("O valor final do seu produto sera: R$ %.2f", resultado);
+}
